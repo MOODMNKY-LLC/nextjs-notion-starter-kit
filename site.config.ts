4eba10743b6914ab1166d7591b7a66cda0e81130
Simeon Bowman <134252285+DOJO-MNKY@users.noreply.github.com>
2024-01-12 01:33:23 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -42,10 +42,10 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
+  pageUrlOverrides: {
+    '/girth': '89407d1153e64884831eae86ed9a26cd',
+    '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  }
   pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
